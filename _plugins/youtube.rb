9df7e96b71af7e7794f06c3a223044e7aca737a1
Manuel Amunategui <amunategui@gmail.com>
2014-10-01 23:05:37 -0700

    better tube

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -1,28 +1,43 @@
-class YouTube < Liquid::Tag
-  Syntax = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
- 
-  def initialize(tagName, markup, tokens)
-    super
- 
-    if markup =~ Syntax then
-      @id = $1
- 
-      if $2.nil? then
-          @width = 560
-          @height = 420
-      else
-          @width = $2.to_i
-          @height = $3.to_i
+# Title: A better YouTube embed tag for Jekyll
+# Author: Tuan Anh Tran <http://tuananh.org>
+# Description: Out put a beautiful thumbnail image. Change to iframe on click.
+# Examples: 
+# {% youtube /v8o-Vd__I-A 560 315 %}
+# {% youtube http://youtu.be/v8o-Vd__I-A %}
+
+module Jekyll
+  class BetterTube < Liquid::Tag
+    @ytid = nil
+    @width = ''
+    @height = ''
+
+    def initialize(tag_name, markup, tokens)
+      if markup =~ /(?:(?:https?:\/\/)?(?:www.youtube.com\/(?:embed\/|watch\?v=)|youtu.be\/)?(\S+)(?:\?rel=\d)?)(?:\s+(\d+)\s(\d+))?/i
+        @ytid = $1
+        @width = $2 || "560"
+        @height = $3 || "315"
       end
-    else
-      raise "No YouTube ID provided in the \"youtube\" tag"
+      super
+    end
+
+    def render(context)
+      ouptut = super
+      if @ytid
+        id = @ytid
+        w = @width
+        h = @height
+        intrinsic = ((h.to_f / w.to_f) * 100)
+        padding_bottom = ("%.2f" % intrinsic).to_s  + "%"
+
+        thumbnail = "<figure class='BetterTube' data-youtube-id='#{id}' data-player-width='#{w}' data-player-height='#{h}' id='#{id}' style='padding-bottom: #{padding_bottom}'><a class='BetterTubePlayer' href='http://www.youtube.com/watch?v=a_426RiwST8' style='background: url(http://img.youtube.com/vi/#{id}/hqdefault.jpg) 50% 50% no-repeat rgb(0, 0, 0);'></a><div class='BetterTube-playBtn'></div>&nbsp;</figure>"
+        
+        video = %Q{#{thumbnail}}
+
+      else
+        "Error while processing. Try: {% youtube video_id [width height] %}"
+      end
     end
   end
- 
-  def render(context)
-    # "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}\" frameborder=\"0\"allowfullscreen></iframe>"
-    "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}?color=white&theme=light\"></iframe>"
-  end
- 
-  Liquid::Template.register_tag "youtube", self
 end
+
+Liquid::Template.register_tag('youtube', Jekyll::BetterTube)
