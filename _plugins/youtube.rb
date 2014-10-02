17254ea63fa3a1047c35824408263d2790b07a8c
Manuel Amunategui <amunategui@gmail.com>
2014-10-01 22:58:21 -0700

    video embedding

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -1,42 +1,28 @@
-# Jekyll - Easy Youtube Embed
-#
-# Katie Harron - https://github.com/pibby
-#
-#   Input:
-#     {% youtube Al9FOtZcadQ %}
-#   Output:
-#   <div class="video">
-#     <figure>
-#       <iframe width="640" height="480" src="//www.youtube.com/embed/Al9FOtZcadQ?rel=0" allowfullscreen></iframe>
-#     </figure>
-#   </div>
-
-module Jekyll
-  class Youtube < Liquid::Tag
-    @url = nil
-
-    VIDEO_URL = /(\S+)/i
-
-    def initialize(tag_name, markup, tokens)
-      super
-
-      if markup =~ VIDEO_URL
-        @url = $1
+class YouTube < Liquid::Tag
+  Syntax = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
+ 
+  def initialize(tagName, markup, tokens)
+    super
+ 
+    if markup =~ Syntax then
+      @id = $1
+ 
+      if $2.nil? then
+          @width = 560
+          @height = 420
+      else
+          @width = $2.to_i
+          @height = $3.to_i
       end
-    end
-
-    def render(context)
-
-      source = "<div class=\"video\">"
-
-      source += "<figure>"
-      source += "<iframe width=\"640\" height=\"480\" src=\"//www.youtube.com/embed/#{@url}?rel=0\" frameborder=\"0\" allowfullscreen></iframe>"
-      source += "</figure>"
-      source += "</div>"
-
-      source
+    else
+      raise "No YouTube ID provided in the \"youtube\" tag"
     end
   end
+ 
+  def render(context)
+    # "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}\" frameborder=\"0\"allowfullscreen></iframe>"
+    "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}?color=white&theme=light\"></iframe>"
+  end
+ 
+  Liquid::Template.register_tag "youtube", self
 end
-
-Liquid::Template.register_tag('youtube', Jekyll::Youtube)
